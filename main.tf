2aeff939eb1837f3995a88380424f0af9a4ed0c7
nerdai <andrei@nerdai.io>
2023-08-07 22:49:30 -0400

    fmt

--- main.tf
+++ main.tf
@@ -8,30 +8,30 @@ data "aws_region" "current" {}
 #Define the VPC
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
-    tags = {
-        Name        = var.vpc_name
-        Environment = "demo_environment"
-        Terraform   = "true"
-    }
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
 }
 #Deploy the private subnets
 resource "aws_subnet" "private_subnets" {
-  for_each = var.private_subnets
-  vpc_id = aws_vpc.vpc.id
+  for_each   = var.private_subnets
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value)
   availability_zone = tolist(data.aws_availability_zones.available.names)[
     each.value
   ]
 
   tags = {
-    Name = each.key
+    Name      = each.key
     Terraform = "true"
   }
 }
 #Deploy the public subnets
 resource "aws_subnet" "public_subnets" {
-  for_each = var.public_subnets
-  vpc_id = aws_vpc.vpc.id
+  for_each   = var.public_subnets
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
   availability_zone = tolist(data.aws_availability_zones.available.names)[
     each.value
@@ -39,7 +39,7 @@ resource "aws_subnet" "public_subnets" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = each.key
+    Name      = each.key
     Terraform = "true"
   }
 }
@@ -47,9 +47,9 @@ resource "aws_subnet" "public_subnets" {
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
-    cidr_block     = "0.0.0.0/0"
-    gateway_id     = aws_internet_gateway.internet_gateway.id
-  #nat_gateway_id = aws_nat_gateway.nat_gateway.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+    #nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
   tags = {
     Name      = "demo_public_rtb"
@@ -60,7 +60,7 @@ resource "aws_route_table" "public_route_table" {
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
-    cidr_block     = "0.0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     # gateway_id     = aws_internet_gateway.internet_gateway.id
     nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
@@ -98,7 +98,7 @@ resource "aws_eip" "nat_gateway_eip" {
   domain     = "vpc"
   depends_on = [aws_internet_gateway.internet_gateway]
   tags = {
-      Name = "demo_igw_eip"
+    Name = "demo_igw_eip"
   }
 }
 
@@ -108,6 +108,6 @@ resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway_eip.id
   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
   tags = {
-      Name = "demo_nat_gateway"
-  } 
+    Name = "demo_nat_gateway"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,25 @@
 variable "aws_region" {
-    type = string
-    default = "ca-central-1"
+  type    = string
+  default = "ca-central-1"
 }
 
 variable "vpc_name" {
-    type = string
-    default = "demo_vpc"
+  type    = string
+  default = "demo_vpc"
 }
 
 variable "private_subnets" {
-    default = {
-        "private_subnet_1" = 1
-        "private_subnet_2" = 2
-        "private_subnet_3" = 3
-    }
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+  }
 }
 
 variable "public_subnets" {
-    default = {
-        "public_subnet_1" = 1
-        "public_subnet_2" = 2
-        "public_subnet_3" = 3
-    }
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
 }
