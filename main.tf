aaff50a8f1a939fe7341d3bd189d219ad596926f
nerdai <andrei@nerdai.io>
2023-08-07 22:56:24 -0400

    validation and change to us-east-1

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "ca-central-1"
+  region = "us-east-1"
 }
 #Retrieve the list of AZs in the current AWS region
 data "aws_availability_zones" "available" {}
--- variables.tf
+++ variables.tf
@@ -1,11 +1,16 @@
 variable "aws_region" {
   type    = string
-  default = "ca-central-1"
+  default = "us-east-1"
 }
 
 variable "vpc_name" {
   type    = string
   default = "demo_vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "private_subnets" {
